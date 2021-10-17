bf6aaa2f40e5eabb1027e47572adccf3c2da9375
GitHub Action <noreply@github.com>
2021-10-17 05:30:37 +0000

    Update sub-web from LM-Firefly/sub-web

---
+++ sub-web/precache-manifest.a70e3f59bda9831ec2eff929fec1a3d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36b676c035bcf27eb30e9dab08c76cd",
+    "url": "_redirects"
+  },
+  {
+    "revision": "385fe425181611b76887",
+    "url": "css/app.3534f01e.css"
+  },
+  {
+    "revision": "680e4cb0b96b1259bd99",
+    "url": "css/chunk-vendors.1019b600.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3b212338f760a893c79e6ec4542b1226",
+    "url": "index.html"
+  },
+  {
+    "revision": "385fe425181611b76887",
+    "url": "js/app.47db0ecd.js"
+  },
+  {
+    "revision": "7d7d9b7940c9e94ac0df",
+    "url": "js/chunk-051deb28.32e4f8ed.js"
+  },
+  {
+    "revision": "680e4cb0b96b1259bd99",
+    "url": "js/chunk-vendors.79abcc0a.js"
+  },
+  {
+    "revision": "417c67dfde0c71bc33e953d4e7d635f8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
